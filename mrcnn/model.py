2ef61e2e53ec39486f709a0dcd5b30e48a448b6c
haruyasu <harukun2002@gmail.com>
2021-08-06 10:10:18 +0900

    model dir

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2264,7 +2264,8 @@ class MaskRCNN():
                 print('Re-starting from epoch %d' % self.epoch)
 
         # Directory for training logs
-        self.log_dir = os.path.join(self.model_dir, "{}{:%Y%m%dT%H%M}".format(self.config.NAME.lower(), now))
+        # self.log_dir = os.path.join(self.model_dir, "{}{:%Y%m%dT%H%M}".format(self.config.NAME.lower(), now))
+        self.log_dir = self.model_dir
 
         # Path to save after each epoch. Include placeholders that get filled by Keras.
         self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(self.config.NAME.lower()))
@@ -2346,7 +2347,7 @@ class MaskRCNN():
                 self.checkpoint_path,
                 verbose=0,
                 save_weights_only=True,
-                period=5
+                # period=5
             ),
         ]
 
